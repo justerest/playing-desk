d0f82d01392a325f7b3f86120d805f8a9514919b
Sergey Klevakin <justerest@yandex.ru>
2017-09-04 21:43:29 +0500

    Хороший тон +

--- src/main.js
+++ src/main.js
@@ -1,17 +1,12 @@
-document.querySelector('button').onclick = takeCard;
-const takedCardsContainer = document.querySelector('.taked-cards');
-
 const CARD_SUITS = ['clubs', 'diamonds', 'hearts', 'spades'];
 const DESK_LENGTH = 36;
-
-const groupLength = DESK_LENGTH / 4;
+const GROUP_LENGTH = DESK_LENGTH / 4;
 
 const desk = new Array(DESK_LENGTH)
   .fill()
   .map((card, i) => {
-    const value = i % groupLength;
+    const value = i % GROUP_LENGTH;
     return {
-      value,
       cost: (
         value === 8 ? 'Ace' :
         value === 7 ? 'King' :
@@ -19,30 +14,30 @@ const desk = new Array(DESK_LENGTH)
         value === 5 ? 'Jack' :
         value + 6
       ),
-      suit: CARD_SUITS[parseInt(i / groupLength)]
+      suit: CARD_SUITS[parseInt(i / GROUP_LENGTH)]
     };
   });
 
+window.onload = () => {
+  const takedCardsContainer = document.querySelector('.taked-cards');
 
-function takeCard() {
-  if (!desk.length) {
-    alert('Карты кончились!');
-    return;
-  }
+  document.querySelector('button').onclick = () => {
+    if (!desk.length) {
+      alert('Карты кончились!');
+      return;
+    }
 
-  const [takedCard] = desk.splice(getRandomInt(0, desk.length), 1);
-  const card = document.createElement('li');
-  card.classList.add(takedCard.cost, takedCard.suit);
-  card.innerHTML = JSON.stringify(takedCard);
-  card.style.backgroundPositionX = '800px';
-  card.style.backgroundPositionY = '1100px';
-  takedCardsContainer.appendChild(card);
+    const [takedCard] = desk.splice(randomInt(0, desk.length), 1);
+    const card = document.createElement('li');
+    card.classList.add(takedCard.cost, takedCard.suit);
+    card.innerHTML = JSON.stringify(takedCard);
+    card.style.backgroundPositionX = '800px';
+    card.style.backgroundPositionY = '1100px';
+    takedCardsContainer.appendChild(card);
+  };
 }
 
-// https://developer.mozilla.org/ru/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-// Возвращает случайное целое число между min (включительно) и max (не включая max)
-// Использование метода Math.round() даст вам неравномерное распределение!
-function getRandomInt(min, max) {
+function randomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min;
 }
 
